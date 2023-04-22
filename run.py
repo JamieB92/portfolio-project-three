610a0b2670ef2e2e904d0054eaa661810e830323
JamieB92 <j.bonner6992@gmail.com>
2023-04-22 19:59:50 +0000

    add clear screen function

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
+import os
+
 def intro():
     print("Welcome to Escape from ISS Ravana")
     print("Escape from ISS Ravana is a text based survival game.")
     print("Where you have to collect all the items within the ship to take on and defeat the intruder and escape with your life!")
     
-    input("Press any key to continue")
+    input("Press any key to continue ")
 
-intro()
 
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
