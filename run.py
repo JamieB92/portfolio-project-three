6355f0908e176f8cd3b5eb5702049c814f88a1cb
JamieB92 <j.bonner6992@gmail.com>
2023-04-22 16:47:06 +0000

    Create introduction function

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def intro():
+    print("Welcome to Escape from ISS Ravana")
+    print("Escape from ISS Ravana is a text based survival game.")
+    print("Where you have to collect all the items within the ship to take on and defeat the intruder and escape with your life!")
+    
+    input("Press any key to continue")
+
+intro()
+
